c26e13cdba0491b181ba2b72449479f2bab2e9d3
Alexandr <98154605+KingSanMon@users.noreply.github.com>
2023-04-15 04:42:14 +0300

    add_date and add_referrer_sistem
    исправление даты в профиле, так же добавлена была система рефералов

--- handlers/users/message.py
+++ handlers/users/message.py
@@ -12,22 +12,30 @@ import time
 async def process_start_command(message: types.Message):
     if db.get("SELECT id FROM users WHERE user_id = ?", (message.from_user.id,)) == None:
         start_command = message.text
+#       получаем айди рефера
         referrer_id = str(start_command[7:])
+#       если ссылки рефера не обнаружено
         if str(referrer_id) != "":
+#           проверка или человек не перешел по свой ссылке
             if str(referrer_id) != str(message.from_user.id):
                 db.change(f"INSERT INTO users VALUES(NULL, ?, ?, ?, ?)", (message.chat.username, int(time.time()), message.from_user.id, referrer_id,))
                 try:
+#                    отправляем пользователю что по его ссылке перешли
                     await bot.send_message(referrer_id, "💎Поздравляю, у вас плюс 1 реферал💎")
                 except:
                     pass
             else:
+#               если пользователь попытался перейти по своей ссылке не регистрируясь при этом выпадает сообщение и регистрирует его как обычного юзера
                 db.change(f"INSERT INTO users VALUES(NULL, ?, ?, ?, NULL)", (message.chat.username, int(time.time()), message.from_user.id,))
-                await bot.send_message(message.from_user.id, "Нельзя зарегистрироваться по собственной ссылке")
+                await bot.send_message(message.from_user.id,
+                                       "❌⚠️Отклонено. Причина:\nпопытка перехода по обственной ссылке⚠️❌"
+                                       )
         else:
+#           если пользователь не переходил по ссылке
             db.change(f"INSERT INTO users VALUES(NULL, ?, ?, ?, NULL)", (message.chat.username, int(time.time()), message.from_user.id,))
     await message.answer(
         f"Приветствуем вас в нашем <b>маркете!</b> Выберете пункт меню, необходимый вам!🤑",
         parse_mode="html",
         reply_markup = start_keyboard
     )
-    print('пользователь: ', message.chat.username, message.chat.first_name, message.chat.last_name, ' нажал старт')
+#     print('пользователь: ', message.chat.username, message.chat.first_name, message.chat.last_name, ' нажал старт')
